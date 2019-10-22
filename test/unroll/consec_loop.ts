89668fe1fe93bbce58f0989a4830e0806d8020f1
seanlatias <seanlatias@gmail.com>
2019-10-22 17:29:36 -0400

    add more samples

---
+++ test/unroll/consec_loop.ts
@@ -0,0 +1,7 @@
+var sum = 0;
+for (let i = 0; i < 10; i = i+1) {
+  sum = sum + i;
+}
+for (let j = 0; j < 10; j = j+1) {
+  sum = sum - j;
+}
---
+++ test/unroll/loop_add.ts
@@ -0,0 +1,5 @@
+var sum = 0;
+for (let i = 0; i < 5; i = i + 1) {
+  sum = sum + i;
+}
+console.log(sum); 
